0782e44e40bc40c8ced53eaf1faa4010d1d01aa4
lihongmei <1224301484@qq.com>
2019-07-24 16:28:53 +0800

    toor_config 提交

---
+++ toor_config/src/main/java/com/lhm/toor_config/ToorConfigApplication.java
@@ -0,0 +1,13 @@
+package com.lhm.toor_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToorConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToorConfigApplication.class, args);
+    }
+
+}
---
+++ toor_config/src/test/java/com/lhm/toor_config/ToorConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lhm.toor_config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ToorConfigApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
